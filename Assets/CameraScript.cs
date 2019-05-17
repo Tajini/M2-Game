8b3e902f487bed534909f414541d10cf1be122bb
Tajini <Sinso7Iblis@hotmail.fr>
2019-05-17 12:00:13 +0200

    First Push

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Heros");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y,-5);
+    
+    }
+}
